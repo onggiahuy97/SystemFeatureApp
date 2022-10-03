db3c9ae765de6cec35295bc7293732366e1ed9cc
Huy Ong <huy.ong@sjsu.edu>
2022-10-02 20:26:19 -0700

    Initial Commit

---
+++ SystemFeatureApp/SystemFeatureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemFeatureAppApp.swift
+//  SystemFeatureApp
+//
+//  Created by Huy Ong on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SystemFeatureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
